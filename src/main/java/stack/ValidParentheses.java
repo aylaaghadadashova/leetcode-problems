d8ef6e7a008b0554633dbfead112ce5e36946e99
Ayla <ayla.aghadadashova.std@bhos.edu.az>
2025-12-20 12:46:45 +0400

    feat: add solution for string/stack problem (20)

---
+++ src/main/java/stack/ValidParentheses.java
@@ -0,0 +1,27 @@
+package stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+                if (ch == ')' && top != '(') return false;
+                if (ch == '}' && top != '{') return false;
+                if (ch == ']' && top != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+
+    }
+
+}
